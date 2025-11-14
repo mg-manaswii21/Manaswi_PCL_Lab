59e82771bdbffad9630767519d269d95a3c5e0af
mg-manaswii21 <thatsmg18@gmail.com>
2025-11-14 17:32:45 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,14 @@
+// Vansh Gaikwad 25070521088 
+
+#include <stdio.h>
+int main(){
+	int inp1 , inp2;
+	scanf("%d" , &inp1);
+	scanf("%d" , &inp2);
+	printf("%d\n" , inp1 + inp2);
+	printf("%d\n" , inp1 - inp2);
+	printf("%d\n" , inp1*inp2);
+	printf("%d\n" , inp1 / inp2);
+	printf("%d\n" , inp1 % inp2);
+	return 0;
+}
